18eb57be89ef2d6fa6f43c56973d83a31e54bdda
vishal kumar <vishal.k3102@gmail.com>
2024-04-20 23:42:04 +0530

    added redux for student  tab of admin dashboard

--- frontend/src/Screens/Admin/Student/AddStudent.jsx
+++ frontend/src/Screens/Admin/Student/AddStudent.jsx
@@ -5,11 +5,8 @@ import { toast } from 'react-hot-toast'
 import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
 import { storage } from '../../../firebase/config'
 import { FiUpload } from 'react-icons/fi'
-import {
-  addAdmin,
-  addStudent,
-  getAllBranch
-} from '../../../Redux/Actions/adminAction'
+import { addStudent, getAllBranch } from '../../../Redux/Actions/adminAction'
+import Loader from '../../../components/Loader'
 
 const AddStudent = () => {
   const [enrollmentNo, setEnrollmentNo] = useState('')
@@ -39,9 +36,9 @@ const AddStudent = () => {
   const addStudentProfile = e => {
     e.preventDefault()
     const formData = {
-      enrollmentNo,
       firstName,
       lastName,
+      enrollmentNo,
       email,
       phoneNumber,
       gender,
@@ -51,172 +48,174 @@ const AddStudent = () => {
       universityRollNo,
       section
     }
-
+    console.log(formData)
     dispatch(addStudent(formData))
     navigate('/admin/home')
   }
 
   return (
-    <form
-      onSubmit={addStudentProfile}
-      className='w-[70%] flex justify-center items-center flex-wrap gap-6 mx-auto mt-10'
-    >
-      <div className='w-[40%]'>
-        <label htmlFor='firstname' className='leading-7 text-sm '>
-          Enter First Name
-        </label>
-        <input
-          type='text'
-          id='firstname'
-          value={firstName}
-          onChange={e => setFirstName(e.target.value)}
-          className='w-full bg-blue-50 rounded border focus:border-dark-green focus:bg-secondary-light focus:ring-2 focus:ring-light-green text-base outline-none py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
-        />
-      </div>
-      <div className='w-[40%]'>
-        <label htmlFor='lastname' className='leading-7 text-sm '>
-          Enter Last Name
-        </label>
-        <input
-          type='text'
-          id='lastname'
-          value={lastName}
-          onChange={e => setLastName(e.target.value)}
-          className='w-full bg-blue-50 rounded border focus:border-dark-green focus:bg-secondary-light focus:ring-2 focus:ring-light-green text-base outline-none py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
-        />
-      </div>
-      <div className='w-[40%]'>
-        <label htmlFor='enrollmentNo' className='leading-7 text-sm '>
-          Enter Enrollment No
-        </label>
-        <input
-          type='text'
-          id='enrollmentNo'
-          value={enrollmentNo}
-          onChange={e => setEnrollmentNo(e.target.value)}
-          className='w-full bg-blue-50 rounded border focus:border-dark-green focus:bg-secondary-light focus:ring-2 focus:ring-light-green text-base outline-none py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
-        />
-      </div>
-      <div className='w-[40%]'>
-        <label htmlFor='email' className='leading-7 text-sm '>
-          Enter Email Address
-        </label>
-        <input
-          type='email'
-          id='email'
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          className='w-full bg-blue-50 rounded border focus:border-dark-green focus:bg-secondary-light focus:ring-2 focus:ring-light-green text-base outline-none py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
-        />
-      </div>
-      <div className='w-[40%]'>
-        <label htmlFor='phoneNumber' className='leading-7 text-sm '>
-          Enter Phone Number
-        </label>
-        <input
-          type='number'
-          id='phoneNumber'
-          value={phoneNumber}
-          onChange={e => setPhoneNumber(e.target.value)}
-          className='w-full bg-blue-50 rounded border focus:border-dark-green focus:bg-secondary-light focus:ring-2 focus:ring-light-green text-base outline-none py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
-        />
-      </div>
-      <div className='w-[40%]'>
-        <label htmlFor='gender' className='leading-7 text-sm '>
-          Select Gender
-        </label>
-        <select
-          id='gender'
-          className='px-2 bg-blue-50 py-3 rounded-sm text-base w-full accent-blue-700 mt-1'
-          value={gender}
-          onChange={e => setGender(e.target.value)}
+    <>
+      {loading === false ? (
+        <form
+          onSubmit={addStudentProfile}
+          className='w-[70%] flex justify-center items-center flex-wrap gap-6 mx-auto mt-10'
         >
-          <option defaultValue>-- Select --</option>
-          <option value='Male'>Male</option>
-          <option value='Female'>Female</option>
-        </select>
-      </div>
-      <div className='w-[40%]'>
-        <label htmlFor='course' className='leading-7 text-sm '>
-          Select Course
-        </label>
-        <select
-          id='course'
-          className='px-2 bg-blue-50 py-3 rounded-sm text-base w-full accent-blue-700 mt-1'
-          value={course}
-          onChange={e => setCourse(e.target.value)}
-        >
-          <option defaultValue>-- Select --</option>
-          <option value='B-Tech'>Bachelor's of Technology</option>
-        </select>
-      </div>
-      <div className='w-[40%]'>
-        <label htmlFor='branch' className='leading-7 text-sm '>
-          Select Branch
-        </label>
-        <select
-          id='branch'
-          className='px-2 bg-blue-50 py-3 rounded-sm text-base w-full accent-blue-700 mt-1'
-          value={branch}
-          onChange={e => setBranch(e.target.value)}
-        >
-          <option defaultValue>-- Select --</option>
-          {branches &&
-            branches.map(i => {
-              return (
-                <option value={i.name} key={i.name}>
-                  {i.name}
-                </option>
-              )
-            })}
-        </select>
-      </div>
-      <div className='w-[40%]'>
-        <label htmlFor='semester' className='leading-7 text-sm '>
-          Select Semester
-        </label>
-        <select
-          id='semester'
-          className='px-2 bg-blue-50 py-3 rounded-sm text-base w-full accent-blue-700 mt-1'
-          value={semester}
-          onChange={e => setSemester(e.target.value)}
-        >
-          <option defaultValue>-- Select --</option>
-          <option value='1'>1st Semester</option>
-          <option value='2'>2nd Semester</option>
-          <option value='3'>3rd Semester</option>
-          <option value='4'>4th Semester</option>
-          <option value='5'>5th Semester</option>
-          <option value='6'>6th Semester</option>
-          <option value='7'>7th Semester</option>
-          <option value='8'>8th Semester</option>
-        </select>
-      </div>
-      <div className='w-[40%]'>
-        <label htmlFor='university-rollno' className='leading-7 text-sm '>
-          Enter University Rollno
-        </label>
-        <input
-          type='text'
-          id='university-rollno'
-          value={universityRollNo}
-          onChange={e => setUniversityRollNo(e.target.value)}
-          className='w-full bg-blue-50 rounded border focus:border-dark-green focus:bg-secondary-light focus:ring-2 focus:ring-light-green text-base outline-none py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
-        />
-      </div>
-      <div className='w-[40%]'>
-        <label htmlFor='section' className='leading-7 text-sm '>
-          Enter Section
-        </label>
-        <input
-          type='text'
-          id='section'
-          value={section}
-          onChange={e => setSection(e.target.value)}
-          className='w-full bg-blue-50 rounded border focus:border-dark-green focus:bg-secondary-light focus:ring-2 focus:ring-light-green text-base outline-none py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
-        />
-      </div>
-      {/* <div className='w-[40%]'>
+          <div className='w-[40%]'>
+            <label htmlFor='firstname' className='leading-7 text-sm '>
+              Enter First Name
+            </label>
+            <input
+              type='text'
+              id='firstname'
+              value={firstName}
+              onChange={e => setFirstName(e.target.value)}
+              className='w-full bg-blue-50 rounded border focus:border-dark-green focus:bg-secondary-light focus:ring-2 focus:ring-light-green text-base outline-none py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+            />
+          </div>
+          <div className='w-[40%]'>
+            <label htmlFor='lastname' className='leading-7 text-sm '>
+              Enter Last Name
+            </label>
+            <input
+              type='text'
+              id='lastname'
+              value={lastName}
+              onChange={e => setLastName(e.target.value)}
+              className='w-full bg-blue-50 rounded border focus:border-dark-green focus:bg-secondary-light focus:ring-2 focus:ring-light-green text-base outline-none py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+            />
+          </div>
+          <div className='w-[40%]'>
+            <label htmlFor='enrollmentNo' className='leading-7 text-sm '>
+              Enter Enrollment No
+            </label>
+            <input
+              type='text'
+              id='enrollmentNo'
+              value={enrollmentNo}
+              onChange={e => setEnrollmentNo(e.target.value)}
+              className='w-full bg-blue-50 rounded border focus:border-dark-green focus:bg-secondary-light focus:ring-2 focus:ring-light-green text-base outline-none py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+            />
+          </div>
+          <div className='w-[40%]'>
+            <label htmlFor='email' className='leading-7 text-sm '>
+              Enter Email Address
+            </label>
+            <input
+              type='email'
+              id='email'
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              className='w-full bg-blue-50 rounded border focus:border-dark-green focus:bg-secondary-light focus:ring-2 focus:ring-light-green text-base outline-none py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+            />
+          </div>
+          <div className='w-[40%]'>
+            <label htmlFor='phoneNumber' className='leading-7 text-sm '>
+              Enter Phone Number
+            </label>
+            <input
+              type='number'
+              id='phoneNumber'
+              value={phoneNumber}
+              onChange={e => setPhoneNumber(e.target.value)}
+              className='w-full bg-blue-50 rounded border focus:border-dark-green focus:bg-secondary-light focus:ring-2 focus:ring-light-green text-base outline-none py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+            />
+          </div>
+          <div className='w-[40%]'>
+            <label htmlFor='gender' className='leading-7 text-sm '>
+              Select Gender
+            </label>
+            <select
+              id='gender'
+              className='px-2 bg-blue-50 py-3 rounded-sm text-base w-full accent-blue-700 mt-1'
+              value={gender}
+              onChange={e => setGender(e.target.value)}
+            >
+              <option defaultValue>-- Select --</option>
+              <option value='Male'>Male</option>
+              <option value='Female'>Female</option>
+            </select>
+          </div>
+          <div className='w-[40%]'>
+            <label htmlFor='course' className='leading-7 text-sm '>
+              Select Course
+            </label>
+            <select
+              id='course'
+              className='px-2 bg-blue-50 py-3 rounded-sm text-base w-full accent-blue-700 mt-1'
+              value={course}
+              onChange={e => setCourse(e.target.value)}
+            >
+              <option defaultValue>-- Select --</option>
+              <option value='B-Tech'>Bachelor's of Technology</option>
+            </select>
+          </div>
+          <div className='w-[40%]'>
+            <label htmlFor='branch' className='leading-7 text-sm '>
+              Select Branch
+            </label>
+            <select
+              id='branch'
+              className='px-2 bg-blue-50 py-3 rounded-sm text-base w-full accent-blue-700 mt-1'
+              value={branch}
+              onChange={e => setBranch(e.target.value)}
+            >
+              <option defaultValue>-- Select --</option>
+              {branches &&
+                branches.map(i => {
+                  return (
+                    <option value={i.name} key={i.name}>
+                      {i.name}
+                    </option>
+                  )
+                })}
+            </select>
+          </div>
+          <div className='w-[40%]'>
+            <label htmlFor='semester' className='leading-7 text-sm '>
+              Select Semester
+            </label>
+            <select
+              id='semester'
+              className='px-2 bg-blue-50 py-3 rounded-sm text-base w-full accent-blue-700 mt-1'
+              value={semester}
+              onChange={e => setSemester(e.target.value)}
+            >
+              <option defaultValue>-- Select --</option>
+              <option value='1'>1st Semester</option>
+              <option value='2'>2nd Semester</option>
+              <option value='3'>3rd Semester</option>
+              <option value='4'>4th Semester</option>
+              <option value='5'>5th Semester</option>
+              <option value='6'>6th Semester</option>
+              <option value='7'>7th Semester</option>
+              <option value='8'>8th Semester</option>
+            </select>
+          </div>
+          <div className='w-[40%]'>
+            <label htmlFor='university-rollno' className='leading-7 text-sm '>
+              Enter University Rollno
+            </label>
+            <input
+              type='text'
+              id='university-rollno'
+              value={universityRollNo}
+              onChange={e => setUniversityRollNo(e.target.value)}
+              className='w-full bg-blue-50 rounded border focus:border-dark-green focus:bg-secondary-light focus:ring-2 focus:ring-light-green text-base outline-none py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+            />
+          </div>
+          <div className='w-[40%]'>
+            <label htmlFor='section' className='leading-7 text-sm '>
+              Enter Section
+            </label>
+            <input
+              type='text'
+              id='section'
+              value={section}
+              onChange={e => setSection(e.target.value)}
+              className='w-full bg-blue-50 rounded border focus:border-dark-green focus:bg-secondary-light focus:ring-2 focus:ring-light-green text-base outline-none py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+            />
+          </div>
+          {/* <div className='w-[40%]'>
         <label htmlFor='file' className='leading-7 text-sm '>
           Select Profile
         </label>
@@ -237,18 +236,22 @@ const AddStudent = () => {
           // onChange={e => setFile(e.target.files[0])}
         />
       </div> */}
-      {/*  {data.profile && (
+          {/*  {data.profile && (
         <div className='w-full flex justify-center items-center'>
           <img src={data.profile} alt='student' className='h-36' />
         </div>
       )} */}
-      <button
-        type='submit'
-        className='bg-blue-500 px-6 py-3 rounded-sm mb-6 text-white'
-      >
-        Add New Student
-      </button>
-    </form>
+          <button
+            type='submit'
+            className='bg-blue-500 px-6 py-3 rounded-sm mb-6 text-white'
+          >
+            Add New Student
+          </button>
+        </form>
+      ) : (
+        <Loader />
+      )}
+    </>
   )
 }
 
