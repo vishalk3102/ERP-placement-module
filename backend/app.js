5523b23826358331a9996e1ed77da48985969941
vishal kumar <vishal.k3102@gmail.com>
2024-04-29 02:35:53 +0530

    cloudinary integration in material file --faculty

--- backend/app.js
+++ backend/app.js
@@ -32,8 +32,8 @@ app.use(
 )
 
 app.use(cookieParser())
-app.use(express.json({ limit: '10mb' }))
-app.use(express.urlencoded({ limit: '10mb', extended: true }))
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
 
 app.use(
   cors({
